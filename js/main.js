c991ba141faefa4cb7927db0fb3a804d094bd7b9
JEANThibaut <thibaut-jean@live.fr>
2021-03-05 16:50:17 +0100

    One function for random word to array

--- js/main.js
+++ js/main.js
@@ -5,71 +5,94 @@ let words= [
     "pizza",
     "internet"
 ];
- let userChoices=[];
- let display=[];
+let userChoices=[];
+let randomWord 
+let display = [];
 
- //Choix ordinateur et décomposition en tableau
+ 
+//---------------------------------Fonctions
+
+//Choix ordinateur et décomposition en tableau
 function randomComputer(){
     let computerChoice = words[Math.floor(Math.random()*words.length)];
-           return computerChoice
-    console.log(computerChoice);
+           return randomWord = Array.from(computerChoice)
 }
 
-function computerToArray(){
-    let computerChoiceArray = Array.from(randomComputer());
-    console.log(computerChoiceArray);
-    return computerChoiceArray
+function fillDisplay(){
+    if(display.length==0){
+            display.fill("_");
+    }
+    else{ 
+        for (var compare of randomWord){
+            for (var i of userChoices){
+                if (compare.includes(i)){
+                display.push(i);
+                    console.log("Lettre trouvée");
+                }
+                else
+                display.push("_");
+                    console.log("Aucune lettre trouvée");
+            }
+        }
+    }    
 }
-
-//---------------------------------Fonctions
-//Comparer les deux choix
 
 
 //Choix de l'utilisateur, vérification et affichage des underscores
 function makeChoice(){
     for (let i=0 ; i<3 ; i++){  
-        let playerChoice = prompt("Entrer une lettre" + display  ); 
+      
         if (isNaN(playerChoice))
             if (playerChoice.length==1){
                         alert("Merci!")
                         return userChoices.push(playerChoice.toLowerCase());
-                        
                     }
-                    else
-                    alert("Merci d'entrer UNE SEULE lettre!");
-        else 
-        alert("Ceci n'est pas une lettre!")        
+                    else{
+                        alert("Merci d'entrer UNE SEULE lettre!");
+                    }
+                        else {
+                        alert("Ceci n'est pas une lettre!")        
+                    }
     }
 }
 
+// //Affichage des caractères
 
+// function displayCaracters(){
+//     if(display.lenght ==0){
+//        display = display.push("_").repeat(randomWord.length);
+//     }
+//     else{ 
+
+//     }
+// }
 
 
 //Déroulement
 
 alert("Bienvenue, vous aller jouer au jeu du pendu");
 randomComputer();
-computerToArray();
-let choice = makeChoice();
+console.log(randomWord);
+fillDisplay();
+let playerChoice = prompt("Entrer une lettre \n"); 
+makeChoice();
 
-
-// if(userChoises.lenght ==0){
-//     display.push("-").repeat(computerChoiceArray.length)
-// }
-    for (var compare of computerToArray()){
-            for (var i of userChoices){
-                if (compare.includes(i)){
-                display.push(i);
-                    //console.log("Lettre trouvée");
-                }
-                else
-                display.push("_");
-                //console.log("Aucune lettre trouvée");
-            }
-    }
 
 console.log(userChoices);
-console.log(display.join(""));
+console.log(display);
+
+// displayCaracters();
+// console.log(display);
+
+
+// computerToArray();
+
+
+
+
+// console.log(userChoices);
+
+ 
 
 
 
