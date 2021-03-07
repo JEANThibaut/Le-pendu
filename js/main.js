cd2987ff0329afff89c37c8003d61b0bf519f17a
JEANThibaut <thibaut-jean@live.fr>
2021-03-07 19:04:18 +0100

    Score OK; Display underscore Ok; bug end game message; Replay to do

--- js/main.js
+++ js/main.js
@@ -9,7 +9,6 @@ let userChoices=[];
 let randomWord 
 let display= [];
 let tries = 7 ;
-
 //---------------------------------Fonctions
 
 //Choix ordinateur et décomposition en tableau
@@ -20,76 +19,71 @@ function randomComputer(){
 
 //Choix de l'utilisateur et vérification 
 function checkChoice(){
-    // for (let j=0; j<=3; j++){
-    //     if (j===3){
-    //     alert("Nous n'avons pas compris votre demande")
-    //     }  
-    //     else if (j<3){
-            for (let i=0 ; i<3 ; i++){  
-                let playerChoice = prompt("Entrer une lettre"+"\n"+ "Il vous reste " + tries + " tentatives" + "\n" + display.join("")); 
-                if (isNaN(playerChoice)){
-                    if (playerChoice.length==1){
-                        return userChoices.push(playerChoice.toLowerCase());
-                    }
-                    else
-                    alert("Merci d'entrer UNE SEULE lettre!");
-                }
-                else
-                alert("Ceci n'est pas une lettre!")        
+    for (let i=0 ; i<3 ; i++){  
+        let playerChoice = prompt("Entrer une lettre"+"\n"+ "Il vous reste " + tries + " tentatives" + "\n" + display.join("")); 
+        if (isNaN(playerChoice)){
+            if (playerChoice.length==1){
+                 return userChoices.push(playerChoice.toLowerCase());
             }
-         }   
-    // }
-// }
-
-// Comparaison et underscore
-//function .indexOf return -1 if false
+            else
+                alert("Merci d'entrer UNE SEULE lettre!");
+            }
+        else
+            alert("Ceci n'est pas une lettre!")        
+        }
+}   
+//remplissage tableau
 function fillDisplay(){
     display = [];
     for (let i=0; i<randomWord.length;i++){
-      if (userChoices.indexOf(randomWord[i])==-1){
-        display.push("_");
-        
-      }
-      else if (userChoices.indexOf(randomWord[i])!==-1){
-       display.push(randomWord[i]);
-      
-      }
+        if (userChoices.indexOf(randomWord[i])==-1){
+            display.push("_");
+        }
+        else if (userChoices.indexOf(randomWord[i])!==-1){
+        display.push(randomWord[i]);
+        }
     }
-    console.log(display.join(""));
+    console.log("display " + display.join(""));
 }
 
-
-    
+//Function Score 
+function score(){
+    let lastItem = userChoices[userChoices.length - 1];
+    let n = randomWord.includes(lastItem)
+        if (!n){
+            tries --
+        }          
+}
 // Fonction boucle de jeu
 function play(){
-    checkChoice();
+    checkChoice();        
+    score();
     fillDisplay();
-    
-}
 
 
-
-
-
+}   
 
 //Déroulement
 
 alert("Bienvenue, vous aller jouer au jeu du pendu");
 randomComputer();
-console.log(randomWord);
+console.log("random "+ randomWord);
 //Init display for first round
 display = new Array(randomWord.length).fill("_");
+
+
 
 while(tries>0){
         if (display.join("")===randomWord.join("")){
             alert("bravo")
-       break
+        break
         }
         
         else if (tries==0){
             alert("dommage")
         
         }
+
         play();
 }
 
